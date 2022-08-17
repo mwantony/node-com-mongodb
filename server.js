730bf24b30f8b99ca585f176a83df6d1f53b8dd3
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-17 14:20:37 -0300

    cap 1

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const http = require("http");
 const port = 3000;
 
+const rotas = {
+  '/': "Curso de Node",
+  '/livros': "Entrei na página de livros",
+  '/autores': "Listagem de autores"
+}
+
 const server = http.createServer((req, res) => {
   res.writeHead(200, {
     "Content-Type": "text/plain",
   });
-  res.end('Curso de Node')
+  res.end(rotas[req.url])
 });
 
 server.listen(port, () => console.log(`Server is running on port ${port}`))
