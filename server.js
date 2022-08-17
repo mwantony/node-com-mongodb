472a47054964199b84873dcb4d3285ddf44add90
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-17 15:46:15 -0300

    cap 2 vid 2 finalizado

--- server.js
+++ server.js
@@ -1,17 +1,7 @@
-const http = require("http");
-const port = 3000;
+import app from './src/app.js'
 
-const rotas = {
-  '/': "Curso de Node",
-  '/livros': "Entrei na página de livros",
-  '/autores': "Listagem de autores"
-}
+const port = process.env.PORT || 3000;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    "Content-Type": "text/plain",
-  });
-  res.end(rotas[req.url])
-});
 
-server.listen(port, () => console.log(`Server is running on port ${port}`))
+
+app.listen(port, () => console.log(`Server is running on port ${port}`))
