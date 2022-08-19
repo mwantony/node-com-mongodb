f03d1c830d5621d8cdd7ec61943c63571b71d681
Antony Maziero Warken <againplayi7@gmail.com>
2022-08-19 14:51:48 -0300

    cap 5 vid 2 finalizado

--- src/controllers/livrosController.js
+++ src/controllers/livrosController.js
@@ -1,55 +1,72 @@
 import livros from "../models/Livro.js";
 class LivroController {
   static listarLivros = (req, res) => {
-    livros.find((err, livros) => {
-      res.json(livros)
-    })
+    livros
+      .find()
+      .populate("autor")
+      .exec((err, livros) => {
+        res.json(livros);
+      });
   };
 
   static listaLivroPorId = (req, res) => {
-    const {idLivro} = req.params
-    livros.findById(idLivro, (err, livro) => {
-      if(!err) {
-        res.status(200).json(livro)
-        return
-      }
-      res.status(404).json({message: err.message `Não foi possível achar o livro de id ${idLivro}`})
-    })
-  }
+    const { idLivro } = req.params;
+    livros
+      .findById(idLivro)
+      .populate("autor", "nome nacionalidade")
+      .exec((err, livro) => {
+        if (!err) {
+          res.status(200).json(livro);
+          return;
+        }
+        res.status(404).json({
+          message: err.message`Não foi possível achar o livro de id ${idLivro}`,
+        });
+      });
+  };
 
   static cadastrarLivro = (req, res) => {
-    let livro = new livros(req.body)
+    let livro = new livros(req.body);
     livro.save((err) => {
-      if(err) {
-        res.status(500).send({message: `${err.message} - falha ao cadastrar livro`})
+      if (err) {
+        res
+          .status(500)
+          .send({ message: `${err.message} - falha ao cadastrar livro` });
       } else {
-        res.status(201).send(livro.toJSON())
+        res.status(201).send(livro.toJSON());
       }
-    })
-  }
+    });
+  };
 
   static atualizarLivro = (req, res) => {
-    const {idLivro} = req.params
-    livros.findByIdAndUpdate(idLivro, {$set: req.body}, (err) => {
-      if(!err) {
-        res.status(200).send({message: `O livro ${idLivro} atualizado com sucesso`})
-        return
+    const { idLivro } = req.params;
+    livros.findByIdAndUpdate(idLivro, { $set: req.body }, (err) => {
+      if (!err) {
+        res
+          .status(200)
+          .send({ message: `O livro ${idLivro} atualizado com sucesso` });
+        return;
       }
-      res.status(500).send({message: err.message `Livro ${idLivro} não atualizado`})
-    })
-
-  }
+      res
+        .status(500)
+        .send({ message: err.message`Livro ${idLivro} não atualizado` });
+    });
+  };
 
   static excluirLivro = (req, res) => {
-    const {idLivro} = req.params
+    const { idLivro } = req.params;
     livros.findByIdAndDelete(idLivro, (err) => {
-      if(!err) {
-        res.status(200).send({message: `Livro de id ${idLivro} deletado com sucesso.`})
-        return
+      if (!err) {
+        res
+          .status(200)
+          .send({ message: `Livro de id ${idLivro} deletado com sucesso.` });
+        return;
       }
-      res.status(500).send({message: err.message`Impossível deletar livro de id ${idLivro}`})
-    })
-  }
+      res.status(500).send({
+        message: err.message`Impossível deletar livro de id ${idLivro}`,
+      });
+    });
+  };
 }
 
 export default LivroController;
